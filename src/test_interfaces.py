7e924248b92e009e9071f0d0b36c1993d9b53f69
coolernato <cmcooling@gmail.com>
2023-06-06 12:02:47 +0100

    Added tests for Task 0

---
+++ src/test_interfaces.py
@@ -0,0 +1,12 @@
+'''Edit the functions in this file as you complete the associated tasks.
+Each test should utilise aspects of your code to complete the specified task.
+These functions will be called by tests in the test suite to ensure you code is working properly.
+'''
+
+'''This function should always return the value zero'''
+def task_0_always_return_0():
+    return(0)
+
+'''This function should return the sum of the parameters a and b'''
+def task_0_addition(a, b):
+    return(a + b)
